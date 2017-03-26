1d4a15094656f0fc8288817e3860c409c7f630af
Maxime Goyette <maxime.g.goyette@gmail.com>
2017-03-26 19:07:19 -0400

    Log PC: instr: memory[1-3] dans debug.txt

--- emulator/test/sfmlEmulator.cpp
+++ emulator/test/sfmlEmulator.cpp
@@ -1,12 +1,15 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include <fstream>
+#include <iomanip>
+
 #include "nes.h"
 #include "rom_file.h"
 
 int main(int argc, char* argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(512, 480), "NES");
+    sf::RenderWindow window(sf::VideoMode(512, 480), "Frankenstein NES Emulator");
     window.setFramerateLimit(30);
     sf::Texture screen;
     sf::Image img;
@@ -19,6 +22,9 @@ int main(int argc, char* argv[])
     Frankenstein::FileRom rom(file);
     Frankenstein::Nes nes(rom);
     
+    std::ofstream out("debug.txt", std::ios::out|std::ios::binary);
+    // Debug info to file
+
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -31,6 +37,19 @@ int main(int argc, char* argv[])
         }
 
         window.clear(sf::Color::Black);
+
+        {
+            auto op = nes.cpu.OpCode();
+            auto instr = nes.cpu.instructions[op];
+            out << std::setfill('0') << std::setw(4) << std::hex << (unsigned int)nes.cpu.registers.PC<< ": ";
+            out << std::setfill(' ') << std::setw(9) << instr.name << ": ";
+            int max = instr.size == 0 ? 3 : instr.size;
+            for(int i = 0; i < max; i++) {
+                out << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)nes.cpu.Operand(i);
+                out << " ";
+            }
+            out << std::endl;
+        }
 
         nes.Step();
         if (nes.cpu.nmiOccurred){
