9496df320d6af04ad3da19040def233cfda7c1a2
Maxime Goyette <maxime.g.goyette@gmail.com>
2017-03-26 21:19:34 -0400

    Ajout de P,A,X,Y au log

--- emulator/test/sfmlEmulator.cpp
+++ emulator/test/sfmlEmulator.cpp
@@ -3,14 +3,17 @@
 
 #include <fstream>
 #include <iomanip>
+#include <sstream>
+#include <chrono>
+#include <bitset>
 
 #include "nes.h"
+#include "cpu.h"
 #include "rom_file.h"
 
 int main(int argc, char* argv[])
 {
     sf::RenderWindow window(sf::VideoMode(512, 480), "Frankenstein NES Emulator");
-    window.setFramerateLimit(30);
     sf::Texture screen;
     sf::Image img;
     sf::Sprite tmp;
@@ -22,8 +25,8 @@ int main(int argc, char* argv[])
     Frankenstein::FileRom rom(file);
     Frankenstein::Nes nes(rom);
     
-    std::ofstream out("debug.txt", std::ios::out|std::ios::binary);
-    // Debug info to file
+    std::ofstream out("debug.txt", std::ios::out | std::ios::binary);
+    out << "EX.TIME|PC  |CZIDBAVS|A |X |Y |Instruction| Hex data" << std::endl;
 
     while (window.isOpen())
     {
@@ -38,20 +41,36 @@ int main(int argc, char* argv[])
 
         window.clear(sf::Color::Black);
 
-        {
-            auto op = nes.cpu.OpCode();
-            auto instr = nes.cpu.instructions[op];
-            out << std::setfill('0') << std::setw(4) << std::hex << (unsigned int)nes.cpu.registers.PC<< ": ";
-            out << std::setfill(' ') << std::setw(9) << instr.name << ": ";
-            int max = instr.size == 0 ? 3 : instr.size;
-            for(int i = 0; i < max; i++) {
-                out << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)nes.cpu.Operand(i);
-                out << " ";
-            }
-            out << std::endl;
+        std::stringstream instString;
+        auto op = nes.cpu.OpCode();
+        auto instr = nes.cpu.instructions[op];
+        instString << std::setfill('0') << std::setw(4) << std::hex
+                   << (unsigned int)nes.cpu.registers.PC << "|";
+        instString << std::bitset<8>(nes.cpu.registers.P) << "|";
+        instString << std::setfill('0') << std::setw(2) << std::hex
+                   << (unsigned int)nes.cpu.registers.A << "|";
+        instString << std::setfill('0') << std::setw(2) << std::hex
+                   << (unsigned int)nes.cpu.registers.X << "|";
+        instString << std::setfill('0') << std::setw(2) << std::hex
+                   << (unsigned int)nes.cpu.registers.Y << "|";
+        instString << std::setfill(' ') << std::setw(11)
+                   << instr.name << "| ";
+
+
+        int max = instr.size == 0 ? 3 : instr.size;
+        for(int i = 0; i < max; i++) {
+            instString << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)nes.cpu.Operand(i);
+            instString << " ";
         }
 
+        auto begin = std::chrono::high_resolution_clock::now();
         nes.Step();
+        auto end = std::chrono::high_resolution_clock::now();
+
+        out << std::setfill(' ') << std::setw(5) << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() << "ns|";
+        out << instString.str();
+        out << std::endl;
+
         if (nes.cpu.nmiOccurred){
             for (unsigned int i = 0; i < 256; ++i) {
                 for (unsigned int j = 0; j < 240; ++j) {
